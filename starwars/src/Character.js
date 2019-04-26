8521c1d9f6eeef6a9a336f672296ad436b23c1b0
chrispetito <christopherpetito@gmail.com>
2019-04-26 10:12:51 -0700

    add styling to character cards and header

--- starwars/src/Character.js
+++ starwars/src/Character.js
@@ -4,22 +4,23 @@ function Character(props) {
   return (
     <div>
       <div id={props.character.id} className="character-card">
-        <h2>{props.character.name}</h2>
-        <ul>
-
-        </ul>
-        <li>
-          <strong>Birth Year: </strong> {props.character.birth_year}
-        </li>
-        <li>
-          <strong>Gender: </strong> {props.character.gender}
-        </li>
-        <li>
-          <strong>Mass: </strong> {props.character.mass}
-        </li>
-        <li>
-          <strong>Eye Color: </strong> {props.character.eye_color}
-        </li>
+        <h2 className='character-name'>{props.character.name}</h2>
+        <div className='individual-list'>
+          <ul>
+            <li>
+              <strong>Birth Year: </strong> {props.character.birth_year}
+            </li>
+            <li>
+              <strong>Gender: </strong> {props.character.gender}
+            </li>
+            <li>
+              <strong>Mass: </strong> {props.character.mass}
+            </li>
+            <li>
+              <strong>Eye Color: </strong> {props.character.eye_color}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
